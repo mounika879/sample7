1d0090b5eba8afe61e10083322b87e78f2120e42
mounika <kodatimounika@gmail.com>
2019-04-02 10:35:51 +0530

    this is sample5

---
+++ src/test/java/sample5/sample5test.java
@@ -0,0 +1,14 @@
+package sample5;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class sample5test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
